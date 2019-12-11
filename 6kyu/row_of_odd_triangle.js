86fc17239353068a591dd806d85992a8e3a5718c
Mabishi Wakio <elizabeth.wakio@andela.com>
2019-12-11 10:42:15 +0300

    Add row of odd triangles kata

---
+++ 6kyu/row_of_odd_triangle.js
@@ -0,0 +1,47 @@
+// https://www.codewars.com/kata/row-of-the-odd-triangle/train/javascript
+
+function oddRow(n) {
+  // const steps_max = [1, ]; // [4,6,8,10] steps.length === n
+  const steps_min = [1, ]
+/*
+  for (let i = 4; steps_max.length < n; i = i+2) {
+    console.log(i)
+    steps_max.push(i);
+  }
+  console.log(steps_max);
+  let max = steps_max.reduce((result, val) => result + val, 0);
+  console.log(max);
+*/
+  for (let i = 2; steps_min.length < n; i = i + 2) {
+    console.log(i)
+    steps_min.push(i);
+  }
+
+  let min = steps_min.reduce((result, val) => result + val, 0);
+  console.log(min)
+
+  // Construct an array where min is the lowest number and max is the highest
+  // and the values are separated by + 2
+  let result = [];
+
+  while (result.length < n) {
+    result.push(min);
+    min = min+2
+  }
+  console.log(result);
+  return result;
+}
+
+
+function oddRow(n) {
+  const min = n * n - (n-1);
+  const max = n * n + (n-1);
+
+  console.log(min);
+  console.log(max);
+
+  return Array(n).fill(0).map((el, idx) => min + idx * 2);
+
+}
+
+console.log(oddRow(3))
