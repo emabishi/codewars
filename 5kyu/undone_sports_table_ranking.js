07afa3e298915caad2ee0e031cf7e94ddfad432b
Mabishi Wakio <elizabeth.wakio@andela.com>
2020-01-07 16:10:25 +0300

    Mark as undone

---
+++ 5kyu/undone_sports_table_ranking.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/5e0baea9d772160032022e8c/train/javascript
+
+function computeRanks(number, games) {
+  // win -> 2 pts
+  // draw -> 1 pt @
+  
+  // compute the order
+}
